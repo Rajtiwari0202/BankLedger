3621d7aa7f4961c4646ee5bcab7ceec656371239
Rajtiwari0202 <rajtiwari16916@gmail.com>
2026-06-10 19:45:17 +0530

    Added ledger, transaction, and account schemas

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -7,24 +7,28 @@ const accountSchema = new mongoose.Schema({
     required: [true, "Account must be associated with a user"],
     index: true
   },
+
   status: {
+    type: String,
     enum: {
       values: ["ACTIVE", "FROZEN", "CLOSED"],
       message: "Status can be either ACTIVE,FROZEN or CLOSED",
     },
+    default: "ACTIVE"
   },
+
   currency: {
     type: String,
     required: [true, "Currency is required for creating an account"],
     default: "INR"
   },
 
-},{
-    timestamps:true
+}, {
+  timestamps: true
 });
 
-accountSchema.index({user:1,status:1})
+accountSchema.index({ user: 1, status: 1 });
 
-const accountModel = mongoose.model("account",accountSchema)
+const accountModel = mongoose.model("account", accountSchema);
 
-module.exports = accountModel
+module.exports = accountModel;
